5d4fabe99167c583526d7e3665121c037a087450
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2026-05-21 10:18:58 +0200

    Update course menu (#47)

--- CourseManager/CourseManager.Client/src/app/course-view/course-view.ts
+++ CourseManager/CourseManager.Client/src/app/course-view/course-view.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { Layout } from '../layout/layout';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-course-view',
   standalone: true,
-  imports: [Layout],
+  imports: [Layout, NgIf],
   templateUrl: './course-view.html',
   styleUrl: './course-view.scss',
 })
 export class CourseView {
-  title = signal('Home');
+  title = signal('Teknisk bananfysik 2026a');
   name: string = 'Teknisk bananfysik 2026a';
+  people: string[] = ['Knatte', 'Bananlars', 'Lotta', 'Bingus', 'Fnöske', 'Tröske'];
   groups: string[] = ['Knatte & Bananlars', 'Lotta & Bingus', 'Fnöske & Tröske'];
+
+ currentview = signal<'groups' | 'participants'>('groups');
+
+  switchView(view: 'groups' | 'participants') {
+    this.currentview.set(view);
+  }
 }
