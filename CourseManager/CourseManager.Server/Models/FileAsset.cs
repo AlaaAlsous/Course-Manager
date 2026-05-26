4296939ad0b6e33e06f1b7f5dd9dbd456de694a6
Alaa Alsous <lawyeralaa@gmail.com>
2026-05-26 11:57:21 +0200

    Create FileAsset EF Core model with storage info and relations to entities

---
+++ CourseManager/CourseManager.Server/Models/FileAsset.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseManager.Server.Models
+{
+    [Index(nameof(FileName))]
+    public class FileAsset
+    {
+        public int FileAssetId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string FileName { get; set; } = null!;
+
+        [Required]
+        public string LocalPath { get; set; } = null!;
+
+        public string? CloudPath { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string StorageProvider { get; set; } = "local";
+
+        [Required]
+        [MaxLength(50)]
+        public string FileType { get; set; } = null!;
+
+        public long FileSize { get; set; }
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<CourseFile> CourseFiles { get; set; } = new List<CourseFile>();
+        public ICollection<CourseEventFile> CourseEventFiles { get; set; } = new List<CourseEventFile>();
+        public ICollection<GroupFile> GroupFiles { get; set; } = new List<GroupFile>();
+        public ICollection<PersonFile> PersonFiles { get; set; } = new List<PersonFile>();
+    }
+}
