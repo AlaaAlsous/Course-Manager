4f58adf76af574f1a97d5cfd1b3a11ad0600c97b
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2026-05-26 17:05:10 +0200

    113 write a file service (#136)
    * Create IFileService.cs
    
    * Create LocalFileService.cs
    
    * Added LocalFileService as a service in program.cs
    
    * minor formatting fix
    
    * Update CourseManager/CourseManager.Server/Program.cs
    
    Co-authored-by: AlexanderLindgren <alexander.lindgren.net25@edu.edugrade.se>
    
    ---------
    
    Co-authored-by: AlexanderLindgren <alexander.lindgren.net25@edu.edugrade.se>

---
+++ CourseManager/CourseManager.Server/FileService/IFileService.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseManager.Server.FileService
+{
+    public interface IFileService
+    {
+        public Task<string?> CreateFileAsync(string filePath, byte[]? fileData);
+        public Task<string> SaveFileAsync(string filePath, byte[] fileData);
+        public Task DeleteFileAsync(string filePath);
+        public Task<byte[]> GetFileAsync(string filePath);
+    }
+}
--- CourseManager/CourseManager.Server/Program.cs
+++ CourseManager/CourseManager.Server/Program.cs
@@ -1,4 +1,17 @@
+using CourseManager.Server.FileService;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IFileService>(sp =>
+{
+    var env = sp.GetRequiredService<IWebHostEnvironment>();
+    var basePath = Path.Combine(env.ContentRootPath, "Data", "Storage");
+    var directory = Directory.CreateDirectory(basePath);
+
+    return new LocalFileService(directory);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
