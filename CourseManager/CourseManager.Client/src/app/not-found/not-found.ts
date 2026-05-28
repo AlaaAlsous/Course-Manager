443627dfb927cb2787d4b61b430d3116ab7585db
Nour Amairy <amairy.nour@gmail.com>
2026-05-28 10:53:16 +0200

    Add: new page not-found.

---
+++ CourseManager/CourseManager.Client/src/app/not-found/not-found.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Layout } from '../layout/layout';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [Layout, RouterLink],
+  templateUrl: './not-found.html',
+  styleUrls: ['./not-found.scss'],
+})
+export class NotFound {
+  title = signal('Sidan hittades inte');
+}
