aac8aafa92fc2e83d8733b02060920741c71108d
Alaa Alsous <lawyeralaa@gmail.com>
2026-07-12 23:40:12 +0200

    Configure routes with public login, protected pages, default redirect

--- CourseManager/CourseManager.Client/src/app/app.routes.ts
+++ CourseManager/CourseManager.Client/src/app/app.routes.ts
@@ -13,30 +13,93 @@ import { PersonCreator } from './person-creator/person-creator';
 import { EditGroup } from './edit-group/edit-group';
 import { NotFound } from './not-found/not-found';
 import { CreateCourseSection } from './create-course-section/create-course-section';
+import { Login } from './login/login';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: Home, data: { title: 'Home' } },
-  { path: 'participants', component: Persons, data: { title: 'Participants' } },
-  { path: 'participants/create', component: PersonCreator, data: { title: 'Create Participant' } },
-  { path: 'participant', component: ParticipantDetail, data: { title: 'Participant Detail' } },
-  { path: 'all-courses', component: AllCourses, data: { title: 'Programs' } },
-  { path: 'all-course-sections', component: AllCourseSections, data: { title: 'Course Sections' } },
-  { path: 'groups/:id', component: GroupView, data: { title: 'Group Detail' } },
-  { path: 'groups/:id/edit', component: EditGroup, data: { title: 'Edit Group' } },
-  { path: 'group', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'create-group', component: CreateGroup, data: { title: 'Create Group' } },
-  { path: 'create-course', component: CreateCourse, data: { title: 'Create Program' } },
+  { path: 'login', component: Login, data: { title: 'Login' } },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  { path: 'home', component: Home, data: { title: 'Home' }, canActivate: [authGuard] },
+  {
+    path: 'participants',
+    component: Persons,
+    data: { title: 'Participants' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'participants/create',
+    component: PersonCreator,
+    data: { title: 'Create Participant' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'participant',
+    component: ParticipantDetail,
+    data: { title: 'Participant Detail' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'all-courses',
+    component: AllCourses,
+    data: { title: 'Programs' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'all-course-sections',
+    component: AllCourseSections,
+    data: { title: 'Course Sections' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'groups/:id',
+    component: GroupView,
+    data: { title: 'Group Detail' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'groups/:id/edit',
+    component: EditGroup,
+    data: { title: 'Edit Group' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'group',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'create-group',
+    component: CreateGroup,
+    data: { title: 'Create Group' },
+    canActivate: [authGuard],
+  },
+  {
+    path: 'create-course',
+    component: CreateCourse,
+    data: { title: 'Create Program' },
+    canActivate: [authGuard],
+  },
   {
     path: 'course/:courseId/course-section/new',
     component: CreateCourseSection,
     data: { title: 'Create Course Section' },
+    canActivate: [authGuard],
   },
   {
     path: 'course/:courseId/course-section/:sectionId',
     component: CourseSectionView,
     data: { title: 'Course Section' },
+    canActivate: [authGuard],
   },
-  { path: 'course/:id', component: CourseView, data: { title: 'Program' } },
+  {
+    path: 'course/:id',
+    component: CourseView,
+    data: { title: 'Program' },
+    canActivate: [authGuard],
+  },
   { path: '**', component: NotFound, data: { title: 'Page not found' } },
 ];
