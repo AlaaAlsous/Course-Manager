82997bc9e37a3925d1bce3e59b188a99e8a368e3
Alaa Alsous <lawyeralaa@gmail.com>
2026-07-12 23:47:39 +0200

    Update layout component with navigation

--- CourseManager/CourseManager.Client/src/app/layout/layout.ts
+++ CourseManager/CourseManager.Client/src/app/layout/layout.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ThemeService } from '../theme.service';
+import { AuthApiService } from '../api-services/auth-api-service';
 
 @Component({
   selector: 'app-layout',
@@ -13,6 +14,8 @@ export class Layout {
     private router: Router,
     public readonly themeService: ThemeService,
   ) {}
+
+  readonly authService = inject(AuthApiService);
 
   @Input() title = '';
 
@@ -37,5 +40,11 @@ export class Layout {
   toggleTheme() {
     this.themeService.toggleTheme();
     this.closeMenu();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.closeMenu();
+    this.router.navigate(['/login']);
   }
 }
